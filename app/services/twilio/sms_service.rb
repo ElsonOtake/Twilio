3ad18934cccb47eb0e953d233981ed82de5db5e9
Elson Otake <elsonotake@yahoo.com.br>
2023-06-28 18:04:36 -0300

    Update sms with parameters

--- app/services/twilio/sms_service.rb
+++ app/services/twilio/sms_service.rb
@@ -3,21 +3,26 @@ module Twilio
     TWILIO_ACCOUNT_SID = ENV['TWILIO_ACCOUNT_SID']
     TWILIO_AUTH_TOKEN = ENV['TWILIO_AUTH_TOKEN']
 
-    def initialize
-
+    def initialize(body:, phone_number:)
+      @body = body
+      @phone_number = phone_number
     end
 
     def call
       client = Twilio::REST::Client.new(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-
-      from = ENV['TWILIO_FROM_NUMBER']
-      to = ENV['MY_MOBILE_NUMBER']
       
       client.messages.create(
-      from: from,
-      to: to,
-      body: "Hey friend!"
+      from: ENV['TWILIO_FROM_NUMBER'],
+      to: to(@phone_number),
+      body: @body
       )
+    end
+
+    private
+
+    def to(phone_number)
+      return ENV['MY_MOBILE_NUMBER'] if Rails.env.development?
+      phone_number
     end
   end
 end
